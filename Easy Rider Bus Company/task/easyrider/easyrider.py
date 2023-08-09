e61423cabfb036aea604194511c8a65611690269
Alejandro RM <alejandror45@gmail.com>
2023-08-09 10:55:04 -0600

    Stage 4/6: Special stops   Find all departure stations, final stops, and transfer stops to fill in the missing specifications.

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,5 +1,6 @@
 import json
 import re
+from collections import Counter
 
 
 def check_errors(buses):
@@ -31,7 +32,55 @@ def check_errors(buses):
     print(f"a_time: {a_time}")
 
 
+def count_stops(buses):
+    counts = {}
+    for bus in buses:
+        if bus["bus_id"] in counts:
+            counts[bus["bus_id"]] += 1
+        else:
+            counts[bus["bus_id"]] = 1
+
+    print(counts)
+
+
 if __name__ == "__main__":
     data = input()
     buses = json.loads(data)
-    check_errors(buses)
+
+    bus_stops = {}
+    stop_names = []
+    starting_stops = set()
+    final_stops = set()
+    transfer_stops = set()
+
+    for stop in buses:
+        bus_id = stop["bus_id"]
+        stop_name = stop["stop_name"]
+        stop_type = stop["stop_type"]
+
+        if bus_id not in bus_stops:
+            bus_stops[bus_id] = []
+
+        bus_stops[bus_id].append((stop_name, stop_type))
+
+    for bus_id, stops in bus_stops.items():
+        stop_types = [stop_type for _, stop_type in stops]
+
+        if "S" not in stop_types or "F" not in stop_types:
+            print(f"There is no start or end stop for the line: {bus_id}")
+            exit()
+
+        starting_stops.add([stop_name for stop_name, stop_type in stops if stop_type == "S"][0])
+        final_stops.add([stop_name for stop_name, stop_type in stops if stop_type == "F"][0])
+        stop_names.append([stop_name for stop_name, _ in stops])
+
+    counters = [Counter(arr) for arr in stop_names]
+    element_counts = Counter()
+    for counter in counters:
+        element_counts.update(counter)
+
+    intersection_elements = [element for element, count in element_counts.items() if count >= 2]
+
+    print("Start stops:", len(starting_stops), sorted(starting_stops))
+    print("Transfer stops:", len(intersection_elements), sorted(intersection_elements))
+    print("Finish stops:", len(final_stops), sorted(final_stops))
